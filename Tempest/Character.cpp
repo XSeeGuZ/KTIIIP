51fa59fd831e65694409d8df6fc59d23429d53dd
TelTRiNoZ <title34718@gmail.com>
2023-03-16 17:52:42 +0700

    Ultimate

--- Tempest/Character.cpp
+++ Tempest/Character.cpp
@@ -13,8 +13,8 @@ Character::Character(int winWidth, int winHeight, int mapW, int mapH) : windowWi
     // worldPos = {static_cast<float>(GetRandomValue(10 * scale, mapWidth - 100)), static_cast<float>(GetRandomValue(6 * scale, mapHeight - 60))};
     worldPos = {800.f, 800.f};
     isEnemy = false;
-    maxHealth = health;
-    speed = 5.f;
+    maxHealth=health;
+
 }
 
 Vector2 Character::getScreenPos()
@@ -49,7 +49,7 @@ void Character::tick(float deltaTime)
             dash_interval = 60;
         }
 
-        if (Dashed == false && Attacking == false)
+        if (Dashed == false && Attacking == false && charging == false)
         {
             if (IsKeyDown(KEY_A))
                 velocity.x -= speed;
@@ -63,7 +63,7 @@ void Character::tick(float deltaTime)
         if (Vector2Length(Vector2Scale(Vector2Normalize(velocity), speed)) != 0)
         {
 
-            if (IsKeyPressed(KEY_SPACE) && Dashed == false && dash_interval == 1 && Attacking == false)
+            if (IsKeyPressed(KEY_SPACE) && Dashed == false && dash_interval == 1 && Attacking == false && charging == false)
             { // Dash Check
                 Dash_Ani_holder = 0;
                 Dash_dir = velocity;
@@ -88,12 +88,12 @@ void Character::tick(float deltaTime)
         }
         if (Dashed == true)
         {
-            worldPos = Vector2Add(worldPos, Vector2Scale(Dash_dir, 5.f));
+            worldPos = Vector2Add(worldPos, Vector2Scale(Dash_dir, Dash_speed));
         }
 
         Vector2 origin{}; // Set origin point of weapon
         Vector2 offset{}; // Set offset of weapon to Character
-        // float rotation{};
+        float rotation{};
         wp_width = weapon.width / max_atk_frames;
         if (rightLeft > 0.f)
         {
@@ -123,20 +123,20 @@ void Character::tick(float deltaTime)
         }
         DrawRectangleLines(getCollisionRec().x, getCollisionRec().y, getCollisionRec().width, getCollisionRec().height, BLUE);
         // Draw Sword when attacking
-        if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || Attacking == true) && Dashed == false && atk_interval <= 0)
+        if ((IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || Attacking == true) && Dashed == false && atk_interval <= 0 && charging == false)
         {
             Attacking = true;
             texture = attack;
             if (atk_ani_holder <= 0)
             {
-                atk_ani_holder = atk_duration;
+                atk_ani_holder = 3;
                 atk_frame++;
                 if (atk_frame >= max_atk_frames)
                 {
-                    Attacking = false;
                     atk_frame = 0;
+                    Attacking = false;
                     texture = idle;
-                    atk_interval = atk_interval_max;
+                    atk_interval = 8;
                 }
             }
             else
@@ -155,10 +155,6 @@ void Character::tick(float deltaTime)
                 DrawRectangleLines(weaponCollisionRec.x, weaponCollisionRec.y, weaponCollisionRec.width, weaponCollisionRec.height, RED); // Hitbox of weapon
                 DrawRectangleLines(getCollisionRec().x, getCollisionRec().y, getCollisionRec().width, getCollisionRec().height, BLUE);
             }
-            else
-            {
-                enemy_dmg_interval = false;
-            }
         }
 
         if (Dashed == true)
@@ -168,29 +164,78 @@ void Character::tick(float deltaTime)
         if (atk_interval > 0)
             atk_interval--;
     }
-    Rectangle healthBarLine{0.f, 0.f, static_cast<float>(windowWidth), windowHeight * 0.09f};
-    Rectangle healthBar{0.f, 0.f, health / maxHealth * windowWidth, windowHeight * 0.09f};
-    DrawRectangleRec(healthBar, RED);
-    DrawRectangleLinesEx(healthBarLine, 5.f, BLACK);
+    Rectangle healthBarLine{0.f, 0.f,static_cast<float>(windowWidth),windowHeight*0.09f};
+    Rectangle healthBar{0.f, 0.f,health/maxHealth*windowWidth,windowHeight*0.09f};
+    DrawRectangleRec(healthBar,RED);
+    DrawRectangleLinesEx(healthBarLine,5.f,BLACK);
+
+    //UltimateBar
+    Rectangle UltiBarLine{getScreenPos().x-(width/2)*scale, getScreenPos().y-20.f, width*scale*2, 20};
+    Rectangle UltiBar{getScreenPos().x-(width/2)*scale, getScreenPos().y-20.f, (Ulti/maxUlti)*width*scale*2, 20};
+    DrawRectangleRec(UltiBar,YELLOW);
+    DrawRectangleLinesEx(UltiBarLine,5.f,BLACK);
+
+    DrawText(std::to_string(Ulti/maxUlti).c_str(),50,200,50,WHITE);
+    if (IsKeyDown(KEY_LEFT_CONTROL) && Ulti<maxUlti && useUlti == false)
+    {
+        charging = true;
+        Ulticharge += deltaTime; 
+        if (Ulticharge > 1.f/20.f )
+        {
+            Ulti++;
+            Ulticharge=0;
+        }
+    }
+    else
+    {
+        charging = false;
+    }
+    if (Ulti==maxUlti && useUlti == false)
+    {
+        Ulticharge=0;
+        useUlti=true;
+        speed+=5;
+        damage+=10;
+        Dash_speed+=2;
+        //atk_duration-=2;
+        //atk_interval_max -= 3;
+    }
+    if (useUlti==true)
+    {
+        Ulticharge+=deltaTime;
+        if (Ulticharge > 1.f/20.f)
+        {
+            Ulti--;
+            Ulticharge=0;
+        }
+        if (Ulti<=0)
+        {
+            useUlti = false;
+            Ulti=0;
+            speed-=5;
+            damage-=10;
+            Dash_speed-=2;
+            //atk_duration+=2;
+            //atk_interval_max += 3;
+        }
+        
+    }
+    
 }
 
 void Character::takeDmg(int dmg, Vector2 kbDirection, float kbMagnitude)
 {
-    if (temp_immune == false)
-    {
-        health -= dmg;
-
-        // Apply knockback
-        Vector2 kbVector = Vector2Normalize(kbDirection);
-        kbVector = Vector2Scale(kbVector, kbMagnitude);
-        worldPos = Vector2Add(worldPos, kbVector);
-    }
-
+    health -= dmg;
     if (health <= 0)
     {
         setAlive(false);
         return;
     }
+
+    // Apply knockback
+    Vector2 kbVector = Vector2Normalize(kbDirection);
+    kbVector = Vector2Scale(kbVector, kbMagnitude);
+    worldPos = Vector2Add(worldPos, kbVector);
 }
 
 void Character::setKB_V(Vector2 enemyScreenPos)
@@ -198,37 +243,39 @@ void Character::setKB_V(Vector2 enemyScreenPos)
     KB_V = Vector2Scale(Vector2Normalize(Vector2Subtract(enemyScreenPos, getScreenPos())), 100.f);
 }
 
-void Character::heal(int size)
-{
-    float s{0.2f * maxHealth};
-    float m{0.5f * maxHealth};
-    float l{0.8f * maxHealth};
+
+void Character::heal(int size){
+    float s{0.2f*maxHealth};
+    float m{0.5f*maxHealth};
+    float l{0.8f*maxHealth};
     switch (size)
     {
     case 1:
-        if ((health + s) >= maxHealth)
-        {
-            health = maxHealth;
-        }
-        else
-        {
-            health += s;
+        if((health+s)>=maxHealth){ 
+            health=maxHealth;
+            }
+        else{
+            health+=s;
         }
         break;
     case 2:
-        if ((health + m) >= maxHealth)
-            health = maxHealth;
+        if((health+m)>=maxHealth)
+            health=maxHealth;
         else
-            health += m;
+            health+=m;
         break;
     case 3:
-        if ((health + l) >= maxHealth)
-            health = maxHealth;
+        if((health+l)>=maxHealth)
+            health=maxHealth;
         else
-            health += l;
+            health+=l;
         break;
     default:
-        health += 0;
+            health+=0;
         break;
     }
 }
+
+
+
+
