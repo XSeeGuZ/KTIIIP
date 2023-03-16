7cd86a2b223e4e7219ed21a95a370c5a53144f00
pubestpubest <110964000+pubestpubest@users.noreply.github.com>
2023-03-16 18:46:48 +0700

    delete old prototype

--- Prototype/tilesets/A2_Ground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="A2_Ground" tilewidth="32" tileheight="32" tilecount="192" columns="16">
- <image source="A2_Ground.png" trans="ff00ff" width="512" height="384"/>
-</tileset>
